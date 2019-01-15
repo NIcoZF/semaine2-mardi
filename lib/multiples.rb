c68d20b6fa186151df8bed80d99d2d1429dc7f6d
NIcoZF <nico-7@hotmail.fr>
2019-01-15 17:12:55 +0100

    multiple test ok

---
+++ lib/multiples.rb
@@ -0,0 +1,8 @@
+def is_multiple_of_3_or_5?(n)
+
+  if n % 3 == 0 || n % 5 == 0
+    return true
+  else
+    return false
+end
+end
---
+++ spec/multiples_spec.rb
@@ -0,0 +1,30 @@
+  require_relative '../lib/multiples'
+
+describe "the is_multiple_of_3_or_5? method" do
+  it "should return TRUE when an integer is a multiple of 3 or 5" do
+    expect(is_multiple_of_3_or_5?(3)).to eq(true)
+    expect(is_multiple_of_3_or_5?(5)).to eq(true)
+    expect(is_multiple_of_3_or_5?(51)).to eq(true)
+    expect(is_multiple_of_3_or_5?(45)).to eq(true)
+  end
+
+  it "should return FALSE when an integer is NOT a multiple of 3 or 5" do
+    expect(is_multiple_of_3_or_5?(2)).to eq(false)
+    expect(is_multiple_of_3_or_5?(7)).to eq(false)
+    expect(is_multiple_of_3_or_5?(64)).to eq(false)
+  end
+end
+
+describe "sum_of_3_and_5_multiples" do
+  it "should return the sum of i multiples of 3 and 5 "do
+    expect(is_multiple_of_3_or_5(10)).to eq(23)
+    expect(is_multiple_of_3_or_5(11)).to eq(33)
+  end
+
+  it "should return error when input is not an integer"do
+  expect(is_multiple_of_3_or_5(-1)).to eq("saisi un entier")
+  expect(is_multiple_of_3_or_5(1,23)).to eq("saisir un entier")
+  expect(is_multiple_of_3_or_5("chiffre")).to eq("saisir un entier")
+
+  end
+end
