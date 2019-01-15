66481a4fcd15ac31cab317e66fa084c9fb8d9108
NIcoZF <nico-7@hotmail.fr>
2019-01-15 18:21:52 +0100

    exo 1 ok

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,8 +1,25 @@
-def is_multiple_of_3_or_5?(n)
-
-  if n % 3 == 0 || n % 5 == 0
+def is_multiple_of_3_or_5?(number)
+  if number % 3 == 0 || number % 5 == 0
     return true
   else
     return false
+  end
 end
+
+def sum_of_3_and_5_multiples(n)
+  unless  n.is_a?(Integer) && n >= 0
+    return "Yo ! Je ne prends que les entiers naturels. TG"
+  else
+    sum = 0
+    number = 0
+    while number < n
+      if is_multiple_of_3_or_5?(number) == true
+          sum += number
+      else
+         sum += 0
+      end
+      number += 1
+    end
+    return sum
+  end
 end
--- spec/multiples_spec.rb
+++ spec/multiples_spec.rb
@@ -17,14 +17,14 @@ end
 
 describe "sum_of_3_and_5_multiples" do
   it "should return the sum of i multiples of 3 and 5 "do
-    expect(is_multiple_of_3_or_5(10)).to eq(23)
-    expect(is_multiple_of_3_or_5(11)).to eq(33)
+    expect(sum_of_3_and_5_multiples(10)).to eq(23)
+    expect(sum_of_3_and_5_multiples(11)).to eq(33)
   end
 
-  it "should return error when input is not an integer"do
-  expect(is_multiple_of_3_or_5(-1)).to eq("saisi un entier")
-  expect(is_multiple_of_3_or_5(1,23)).to eq("saisir un entier")
-  expect(is_multiple_of_3_or_5("chiffre")).to eq("saisir un entier")
+  it "should return error when input is not an integer" do
+  expect(sum_of_3_and_5_multiples(-1)).to eq("Yo ! Je ne prends que les entiers naturels. TG")
+  expect(sum_of_3_and_5_multiples(1.23)).to eq("Yo ! Je ne prends que les entiers naturels. TG")
+  expect(sum_of_3_and_5_multiples("chiffre")).to eq("Yo ! Je ne prends que les entiers naturels. TG")
 
   end
 end
